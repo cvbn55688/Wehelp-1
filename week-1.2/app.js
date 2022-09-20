39d663a6a09db28f880d144d2db86862da291301
cvbn55688 <109027415+cvbn55688@users.noreply.github.com>
2022-09-20 21:05:44 +0800

    Add files via upload

--- week-1.2/app.js
+++ week-1.2/app.js
@@ -1,16 +1,52 @@
-document.addEventListener("click",clickHidden);
-let label = document.querySelector("label");
+  //獲取當前螢幕解析度
+// console.log(winWide)
+// let wideScreen = false;
 let mn = document.querySelector(".active");
+document.addEventListener("click",clickHidden);
 
+window.addEventListener("resize",function(wide){
+    winWide = window.innerWidth;
+    let mn = document.querySelector(".active");
+    
+    if (winWide <= 600){
+        mn.style.transform="scaleX(0)";
+    }else{
+        mn.style.transform="scaleX(1)";
+    }
+});
+
+winWide = window.innerWidth;
+console.log(winWide)
 function clickHidden(eve){
-    console.log(eve.target.className, eve.target.tagName)
-
-    if (eve.target.tagName == "LI" || eve.target.tagName == "INPUT" || eve.target.tagName == "UL"){
-        mn.style.transform="scaleX(1)"
-        console.log("pass")
+    if(winWide <= 600){        
+        if (eve.target.tagName == "LI" || eve.target.tagName == "INPUT" || eve.target.tagName == "UL"){
+                mn.style.transform="scaleX(1)";
+                console.log("pass");
+        }else{
+            mn.style.transform="scaleX(0)";
+        }            
     }
 
-    else{
-        mn.style.transform="scaleX(0)"
-    }
 }
+
+
+// document.addEventListener("click",clickHidden);
+// let mn = document.querySelector(".active");
+// function clickHidden(eve){
+//     // console.log(eve.target.className, eve.target.tagName)
+//     if(winWide <= 600){ 
+//         if (eve.target.tagName == "LI" || eve.target.tagName == "INPUT" || eve.target.tagName == "UL"){
+//             mn.style.transform="scaleX(1)";
+//             console.log("pass");
+//         }
+
+        
+//     }
+// }
+
+
+
+
+
+
+
