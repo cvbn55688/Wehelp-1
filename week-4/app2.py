d04094e988ce55839e92bbd8732529d2af0bf70b
cvbn55688 <109027415+cvbn55688@users.noreply.github.com>
2022-10-13 14:18:55 +0800

    Add files via upload

---
+++ week-4/app2.py
@@ -0,0 +1,58 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask import redirect
+from flask import make_response
+
+app = Flask(
+    __name__,
+    static_folder="static",
+    static_url_path="/"
+)
+
+app.secret_key="test"
+
+@app.route("/") #首頁
+def index():
+    resp = make_response(render_template("index.html"))
+    resp.set_cookie(key="isLogin", value="", expires=0) #先清除使用者資料
+    return resp
+
+@app.route("/signin", methods=["POST"]) #驗證帳密
+def signin():
+    account = request.form["account"]
+    password = request.form["password"]
+    if account == "test" and password == "test":
+        resp = make_response(redirect ("/member"))
+        resp.set_cookie(key="isLogin", value="True") #設定cookie
+        return resp
+    elif account == "" or password == "":
+        return redirect ("/error?message=請輸入帳號、密碼")
+    else:
+        return redirect ("/error?message=帳號、或密碼輸入錯誤")
+
+@app.route("/member") #會員頁
+def member():
+    loginCookie = request.cookies.get("isLogin")
+    if loginCookie == "True": #判斷是否已登入
+        return render_template("member.html")
+    else:
+        return redirect("/")
+
+@app.route("/error") #錯誤頁
+def error():
+    errorMes = request.args.get("message", "")
+    return render_template("error.html", errorMes = errorMes) #回傳error訊息
+
+@app.route("/signout") #登出
+def signout():
+    resp = make_response(redirect ("/")) #清除cookie的登入狀態
+    resp.set_cookie(key="isLogin", value="", expires=0) #把時限設定為0就可以把cookie刪掉
+    return resp
+
+@app.route("/square/<num>")
+def square(num):
+    num = int(num)**2
+    return render_template("square.html", ans = num)
+
+app.run(port = 3000, debug = True)
